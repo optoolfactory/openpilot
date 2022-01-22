9937b35f5bb518bf0c84c6c1ea5f54d11807d253
multikyd <72329880+multikyd@users.noreply.github.com>
2022-01-22 13:27:24 +0900

    kyd

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1170,15 +1170,15 @@ static void draw_compass(UIState *s) {
       ui_draw_text(s, rect.centerX(), rect.centerY()-16, "NW", 45, COLOR_GREEN_ALPHA(200), "sans-bold");
     }
     ui_draw_text(s, rect.centerX(), rect.centerY()+16, degree, 40, COLOR_WHITE_ALPHA(200), "sans-bold");
-    float niddle_rotation = s->scene.bearingUblox/180*3.141592;
-    nvgSave(s->vg);
-    nvgTranslate(s->vg, compass_x+compass_size/2, compass_y+compass_size/2);
-    nvgRotate(s->vg, niddle_rotation);
-    nvgFontFace(s->vg, "sans-bold");
-    nvgFontSize(s->vg, 70);
-    nvgFillColor(s->vg, COLOR_RED_ALPHA(200));
-    nvgText(s->vg, 50*sin(s->scene.bearingUblox), 50*cos(s->scene.bearingUblox), "^", NULL);
-    nvgRestore(s->vg);
+    //float niddle_rotation = s->scene.bearingUblox/180*3.141592;
+    //nvgSave(s->vg);
+    //nvgTranslate(s->vg, compass_x+compass_size/2, compass_y+compass_size/2);
+    //nvgRotate(s->vg, niddle_rotation);
+    //nvgFontFace(s->vg, "sans-bold");
+    //nvgFontSize(s->vg, 70);
+    //nvgFillColor(s->vg, COLOR_RED_ALPHA(200));
+    //nvgText(s->vg, 50*sin(s->scene.bearingUblox), 50*cos(s->scene.bearingUblox), "^", NULL);
+    //nvgRestore(s->vg);
   }
 }
 
