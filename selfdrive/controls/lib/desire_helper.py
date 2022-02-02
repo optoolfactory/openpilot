a23b51a42ecdd36b76d0e32d94c618c57bd45985
multikyd <72329880+multikyd@users.noreply.github.com>
2022-02-02 10:28:19 +0900

    kyd

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -1,6 +1,7 @@
 from cereal import log
 from common.realtime import DT_MDL
 from selfdrive.config import Conversions as CV
+from common.numpy_fast import interp
 from common.params import Params
 from decimal import Decimal
 
