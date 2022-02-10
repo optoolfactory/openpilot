43bfe9bd3c1f372c836c1fbb0f8d435181d355cc
multikyd <72329880+multikyd@users.noreply.github.com>
2022-02-10 15:02:26 +0900

    kyd

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -636,14 +636,14 @@ class CarController():
               self.keep_decel_on = False
               self.change_accel_fast = False
             elif aReqValue > 0.0:
-              stock_weight = interp(CS.lead_distance, [3.5, 8.0, 15.0, 25.0, 30.0], [0.3, 1.0, 1.0, 0.0, 1.0])
+              stock_weight = interp(CS.lead_distance, [3.5, 8.0, 15.0, 25.0, 30.0], [0.1, 1.0, 1.0, 0.0, 1.0])
               accel = accel * (1.0 - stock_weight) + aReqValue * stock_weight
             elif aReqValue < 0.0 and CS.lead_distance <= 4.3 and -5 < lead_objspd and accel > aReqValue:
               accel = self.accel - (DT_CTRL * clip(CS.out.vEgo*0.9, 1.0, 3.0))
             elif aReqValue < 0.0 and lead_objspd < -15:
               accel = (aReqValue + accel) / 2
             elif aReqValue < 0.0:
-              stock_weight = interp(CS.lead_distance, [5.0, 9.0, 20.0, 25.0, 30.0], [0.0, 1.0, 1.0, 0.0, 1.0])
+              stock_weight = interp(CS.lead_distance, [5.0, 9.0, 20.0, 25.0, 30.0], [0.0, 0.8, 1.0, 0.0, 1.0])
               accel = accel * (1.0 - stock_weight) + aReqValue * stock_weight
             else:
               stock_weight = 0.0
