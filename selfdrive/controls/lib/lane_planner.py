ff3c7445116051802bb4fbf6d9ca7f71fe0085ef
multikyd <72329880+multikyd@users.noreply.github.com>
2022-02-18 16:00:08 +0900

    kyd

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -7,6 +7,7 @@ from selfdrive.hardware import EON, TICI
 from selfdrive.swaglog import cloudlog
 from common.params import Params
 from decimal import Decimal
+import common.log as trace1
 
 TRAJECTORY_SIZE = 33
 # camera offset is meters from center car to camera
@@ -153,4 +154,9 @@ class LanePlanner:
       path_xyz[:,1] = self.d_prob * lane_path_y_interp + (1.0 - self.d_prob) * path_xyz[:,1]
     else:
       cloudlog.warning("Lateral mpc - NaNs in laneline times, ignoring")
+
+    str_log = 'L/R/W={:.1f}/{:.1f}/{:.1f}  C/E/S={:.1f}/{:.1f}/{:.1f}  SI={}'.format(self.lll_y[0], self.rll_y[0], current_lane_width, \
+     self.lane_width_certainty.x, self.lane_width_estimate.x, speed_lane_width, safe_idxs[0])
+    trace1.printf3('{}'.format(str_log))
+      
     return path_xyz
