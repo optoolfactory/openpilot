baaa05a9b600aa335f99bda7744665c324b544a1
multikyd <72329880+multikyd@users.noreply.github.com>
2022-03-05 17:43:46 +0900

    kyd

--- selfdrive/manager/build.py
+++ selfdrive/manager/build.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import sys
 import time
+import datetime
 import textwrap
 from pathlib import Path
 
@@ -28,62 +29,69 @@ def build(spinner: Spinner, dirty: bool = False) -> None:
   nproc = os.cpu_count()
   j_flag = "" if nproc is None else f"-j{nproc - 1}"
 
-  for retry in [True, False]:
-    scons = subprocess.Popen(["scons", j_flag, "--cache-populate"], cwd=BASEDIR, env=env, stderr=subprocess.PIPE)
-    assert scons.stderr is not None
+  # for retry in [True, False]:
+  scons = subprocess.Popen(["scons", j_flag, "--cache-populate"], cwd=BASEDIR, env=env, stderr=subprocess.PIPE)
+  assert scons.stderr is not None
 
-    compile_output = []
+  compile_output = []
 
-    # Read progress from stderr and update spinner
-    while scons.poll() is None:
-      try:
-        line = scons.stderr.readline()
-        if line is None:
-          continue
-        line = line.rstrip()
+  start = time.time()
+  # Read progress from stderr and update spinner
+  while scons.poll() is None:
+    try:
+      line = scons.stderr.readline()
+      if line is None:
+        continue
+      line = line.rstrip()
 
-        prefix = b'progress: '
-        if line.startswith(prefix):
-          i = int(line[len(prefix):])
-          spinner.update_progress(MAX_BUILD_PROGRESS * min(1., i / TOTAL_SCONS_NODES), 100.)
-        elif len(line):
-          compile_output.append(line)
-          print(line.decode('utf8', 'replace'))
-      except Exception:
-        pass
+      prefix = b'progress: '
+      if line.startswith(prefix):
+        i = int(line[len(prefix):])
+        elapsed = time.time() - start
+        elapsed_time = str(datetime.timedelta(seconds=elapsed))
+        elapsed_out = elapsed_time[2:7]
+        scons_node = str(i) + " / " + str(TOTAL_SCONS_NODES)
+        str_out = "Elapsed: " + str(elapsed_out) + "      Compiled: " + str(scons_node)
+        spinner.update(str_out)
+      elif len(line):
+        compile_output.append(line)
+        print(line.decode('utf8', 'replace'))
+    except Exception:
+      pass
 
-    if scons.returncode != 0:
-      # Read remaining output
-      r = scons.stderr.read().split(b'\n')
-      compile_output += r
+  if scons.returncode != 0:
+    # Read remaining output
+    r = scons.stderr.read().split(b'\n')
+    compile_output += r
 
-      if retry and (not dirty):
-        if not os.getenv("CI"):
-          print("scons build failed, cleaning in")
-          for i in range(3, -1, -1):
-            print("....%d" % i)
-            time.sleep(1)
-          subprocess.check_call(["scons", "-c"], cwd=BASEDIR, env=env)
-        else:
-          print("scons build failed after retry")
-          sys.exit(1)
-      else:
-        # Build failed log errors
-        errors = [line.decode('utf8', 'replace') for line in compile_output
-                  if any(err in line for err in [b'error: ', b'not found, needed by target'])]
-        error_s = "\n".join(errors)
-        add_file_handler(cloudlog)
-        cloudlog.error("scons build failed\n" + error_s)
+    # if retry and (not dirty):  # I want to check what errors are immediately.
+    #   if not os.getenv("CI"):
+    #     print("scons build failed, cleaning in")
+    #     for i in range(3, -1, -1):
+    #       print("....%d" % i)
+    #       time.sleep(1)
+    #     subprocess.check_call(["scons", "-c"], cwd=BASEDIR, env=env)
+    #   else:
+    #     print("scons build failed after retry")
+    #     sys.exit(1)
+    # else:
 
-        # Show TextWindow
-        spinner.close()
-        if not os.getenv("CI"):
-          error_s = "\n \n".join("\n".join(textwrap.wrap(e, 65)) for e in errors)
-          with TextWindow("openpilot failed to build\n \n" + error_s) as t:
-            t.wait_for_exit()
-        exit(1)
-    else:
-      break
+    # Build failed log errors
+    errors = [line.decode('utf8', 'replace') for line in compile_output
+              if any(err in line for err in [b'error: ', b'not found, needed by target'])]
+    error_s = "\n".join(errors)
+    add_file_handler(cloudlog)
+    cloudlog.error("scons build failed\n" + error_s)
+
+    # Show TextWindow
+    spinner.close()
+    if not os.getenv("CI"):
+      error_s = "\n \n".join("\n".join(textwrap.wrap(e, 65)) for e in errors)
+      with TextWindow("openpilot failed to build\n \n" + error_s) as t:
+        t.wait_for_exit()
+    exit(1)
+  # else:
+  #   break
 
   # enforce max cache size
   cache_files = [f for f in CACHE_DIR.rglob('*') if f.is_file()]
@@ -98,5 +106,5 @@ def build(spinner: Spinner, dirty: bool = False) -> None:
 
 if __name__ == "__main__" and not PREBUILT:
   spinner = Spinner()
-  spinner.update_progress(0, 100)
+  spinner.update("Openpilot Compiling...")
   build(spinner, is_dirty())
