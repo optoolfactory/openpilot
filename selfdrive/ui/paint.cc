aa939939cb3c2251315becbfdad378838d1f8b66
multikyd <72329880+multikyd@users.noreply.github.com>
2022-03-25 23:02:58 +0900

    kyd

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -737,8 +737,8 @@ static int bb_ui_draw_measure(UIState *s, const char* bb_value, const char* bb_u
 
     nvgBeginPath(s->vg);
     nvgMoveTo(s->vg, bb_x-80, bb_y+90);
-    nvgLineTo(s->vg, bb_x-80+(min(num_value, 6400)*0.0125), bb_y-90+(min(num_value, 6400)*0.005));
-    nvgLineTo(s->vg, bb_x-80+(min(num_value, 6400)*0.0125), bb_y+90);
+    nvgLineTo(s->vg, bb_x-80+((int)fmin(num_value, 6400)*0.0125), bb_y-90+((int)fmin(num_value, 6400)*0.005));
+    nvgLineTo(s->vg, bb_x-80+((int)fmin(num_value, 6400)*0.0125), bb_y+90);
     nvgLineTo(s->vg, bb_x-80, bb_y+90);
     nvgClosePath(s->vg);
     NVGpaint rpm_gradient = nvgLinearGradient(s->vg, bb_x-80, bb_y+90, bb_x+80, bb_y+30, COLOR_GREEN_ALPHA(180), COLOR_RED_ALPHA(230));
