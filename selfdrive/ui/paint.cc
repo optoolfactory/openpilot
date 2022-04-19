131a6d57ff46868a1e4d5e00963d234c772e3cc1
multikyd <72329880+multikyd@users.noreply.github.com>
2022-04-20 00:19:55 +0900

    kyd

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1656,29 +1656,30 @@ static void ui_draw_auto_hold(UIState *s) {
 }
 
 static void ui_draw_rpm_animation(UIState *s) {
-  const int center_x = s->fb_w/2;
-  const int center_y = 250;
-  const int radius_i = 140;
-  const int radius_o = 185;
+  float center_x = 960.0f;
+  float center_y = 250.0f;
+  float radius_i = 140.0f;
+  float radius_o = 185.0f;
   //int rpm = scene.engine_rpm;
-  float rpm = 3600;
+  float rpm = 1700.0f;
   // yp = y0 + ((y1-y0)/(x1-x0)) * (xp - x0),  yp = interp(xp, [x0, x1], [y0, y1])
-  float rpm_to_deg = round(9 + ((27-9)/(3600-0)) * (rpm - 0)); // min:9, max:27
+  float rpm_to_deg = round(9.0f + ((27.0f-9.0f)/(3600.0f-0.0f)) * (rpm - 0.0f)); // min:9, max:27
   //int rpm_to_deg = 27;
+  float target = (float)(NVG_PI / 12.0f)*rpm_to_deg;
 
   nvgBeginPath(s->vg);
-  nvgMoveTo(s->vg, center_x-(radius_i*fabs(cos(NVG_PI/4))), center_y+(radius_i*fabs(sin(NVG_PI/4))));
-  nvgLineTo(s->vg, center_x-(radius_o*fabs(cos(NVG_PI/4))), center_y+(radius_o*fabs(sin(NVG_PI/4))));
-  nvgArc(s->vg, center_x, center_y, radius_o, (NVG_PI/12)*9, (NVG_PI/12)*rpm_to_deg, NVG_CW);
+  //nvgMoveTo(s->vg, center_x-(radius_i*fabs(cos(NVG_PI/4))), center_y+(radius_i*fabs(sin(NVG_PI/4))));
+  //nvgLineTo(s->vg, center_x-(radius_o*fabs(cos(NVG_PI/4))), center_y+(radius_o*fabs(sin(NVG_PI/4))));
+  nvgArc(s->vg, center_x, center_y, radius_i, (NVG_PI/12.0f)*9.0f, target, NVG_CW);
   //nvgArc(s->vg, center_x, center_y, radius_o, (NVG_PI/12)*9, (NVG_PI/12)*27, NVG_CW);
   //nvgLineTo(s->vg, center_x+(radius_i*fabs(cos(NVG_PI/4))), center_y+(radius_i*fabs(sin(NVG_PI/4))));
-  nvgLineTo(s->vg, center_x+(radius_i*cos((NVG_PI/12)*rpm_to_deg)), center_y+(radius_i*sin((NVG_PI/12)*rpm_to_deg)));
-  nvgArc(s->vg, center_x, center_y, radius_i, (NVG_PI/12)*rpm_to_deg, (NVG_PI/12)*9, NVG_CCW);
+  //nvgLineTo(s->vg, center_x+(radius_i*cos((NVG_PI/12)*rpm_to_deg)), center_y+(radius_i*sin((NVG_PI/12)*rpm_to_deg)));
+  nvgArc(s->vg, center_x, center_y, radius_o, target, (NVG_PI/12.0f)*9.0f, NVG_CCW);
   nvgClosePath(s->vg);
-  nvgStrokeWidth(s->vg, 1);
-  nvgStroke(s->vg);
-  // nvgFillColor(s->vg, nvgRGBA(255,128,0,150));
-  // nvgFill(s->vg);
+  //nvgStrokeWidth(s->vg, 1);
+  //nvgStroke(s->vg);
+  nvgFillColor(s->vg, nvgRGBA(255,128,0,150));
+  nvgFill(s->vg);
 }
 
 static void ui_draw_grid(UIState *s) {
