e2b862e6efb24ff51d5bcfb3d6d88f466df9ebee
multikyd <72329880+multikyd@users.noreply.github.com>
2022-04-19 23:56:06 +0900

    kyd

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1669,10 +1669,10 @@ static void ui_draw_rpm_animation(UIState *s) {
   nvgBeginPath(s->vg);
   nvgMoveTo(s->vg, center_x-(radius_i*fabs(cos(NVG_PI/4))), center_y+(radius_i*fabs(sin(NVG_PI/4))));
   nvgLineTo(s->vg, center_x-(radius_o*fabs(cos(NVG_PI/4))), center_y+(radius_o*fabs(sin(NVG_PI/4))));
-  nvgArc(s->vg, center_x, center_y, radius_o, NVG_PI / (12 * 9), NVG_PI / (12 * rpm_to_deg), NVG_CW);
+  nvgArc(s->vg, center_x, center_y, radius_o, (NVG_PI/12)*9, (NVG_PI/12)*rpm_to_deg, NVG_CW);
   nvgLineTo(s->vg, center_x+(radius_i*fabs(cos(NVG_PI/4))), center_y+(radius_i*fabs(sin(NVG_PI/4))));
   //nvgLineTo(s->vg, center_x+(radius_i*cos(NVG_PI/12*rpm_to_deg)), center_y+(radius_i*sin(NVG_PI/12*rpm_to_deg)));
-  nvgArc(s->vg, center_x, center_y, radius_i, NVG_PI / (12 * rpm_to_deg), NVG_PI / (12 * 9), NVG_CCW);
+  nvgArc(s->vg, center_x, center_y, radius_i, (NVG_PI/12)*rpm_to_deg, (NVG_PI/12)*9, NVG_CCW);
   nvgClosePath(s->vg);
   nvgStrokeWidth(s->vg, 1);
   nvgStroke(s->vg);
