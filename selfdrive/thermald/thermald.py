27bfb0d9907e18a83f526a9039843e8b2e5ab6f7
multikyd <72329880+multikyd@users.noreply.github.com>
2022-04-20 22:13:52 +0900

    kyd

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -289,7 +289,8 @@ def thermald_thread() -> NoReturn:
     if (count % int(10. / DT_TRML)) == 0:
       try:
         network_type = HARDWARE.get_network_type()
-        network_strength, connect_name, rsrp = HARDWARE.get_network_strength(network_type)
+        if EON:
+          network_strength, connect_name, rsrp = HARDWARE.get_network_strength(network_type)
         network_info = HARDWARE.get_network_info()  # pylint: disable=assignment-from-none
         if TICI:
           nvme_temps = HARDWARE.get_nvme_temperatures()
