73fab2645d657b2b4c17fdcac598600231ee2ddc
multikyd <72329880+multikyd@users.noreply.github.com>
2022-04-20 21:41:33 +0900

    kyd

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1663,8 +1663,8 @@ static void ui_draw_rpm_animation(UIState *s) {
   float radius_o = 185.0f;
   char gearstep_str[3];
   char rpm_str[6];
-  //float rpm = fmin(s->scene.engine_rpm, 3600.0f);
-  float rpm = 3600.0f;
+  float rpm = fmin(s->scene.engine_rpm, 3600.0f);
+  //float rpm = 3600.0f;
   // yp = y0 + ((y1-y0)/(x1-x0)) * (xp - x0),  yp = interp(xp, [x0, x1], [y0, y1])
   float rpm_to_deg = floor(9.0f + ((27.0f-9.0f)/(3600.0f-0.0f)) * (rpm - 0.0f)); // min:9, max:27
   float target1 = (float)(NVG_PI/12.0f)*9.0f;
@@ -1701,7 +1701,7 @@ static void ui_draw_rpm_animation(UIState *s) {
   snprintf(gearstep_str, sizeof(gearstep_str), "%d", s->scene.gear_step);
   ui_draw_text(s, center_x, center_y-90, gearstep_str, 55, COLOR_WHITE_ALPHA(200), "sans-semibold");
   snprintf(rpm_str, sizeof(rpm_str), "%.0f", s->scene.engine_rpm);
-  ui_draw_text(s, center_x, center_y-120, s->scene.engine_rpm>1?rpm_str:"2500", 50, COLOR_WHITE_ALPHA(200), "sans-semibold");
+  ui_draw_text(s, center_x, center_y-120, s->scene.engine_rpm>1?rpm_str:"", 45, COLOR_WHITE_ALPHA(200), "sans-semibold");
 }
 
 // grid line by opkr for mounting device appropriate position  BF:Back and Forth Angle, RL:Right and Left Angle
