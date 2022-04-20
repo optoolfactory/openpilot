bfe492ec81e6325fac235aa7b9f68c476449804f
multikyd <72329880+multikyd@users.noreply.github.com>
2022-04-20 23:15:54 +0900

    kyd

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -358,15 +358,15 @@ void panda_state_thread() {
 
     if (Hardware::TICI()) {
       double read_time = millis_since_boot();
-      ps.setVoltage(std::stoi(util::read_file("/sys/class/hwmon/hwmon1/in1_input")));
-      ps.setCurrent(std::stoi(util::read_file("/sys/class/hwmon/hwmon1/curr1_input")));
+      ps.setVoltage(std::atoi(util::read_file("/sys/class/hwmon/hwmon1/in1_input").c_str()));
+      ps.setCurrent(std::atoi(util::read_file("/sys/class/hwmon/hwmon1/curr1_input").c_str()));
       read_time = millis_since_boot() - read_time;
       if (read_time > 50) {
         LOGW("reading hwmon took %lfms", read_time);
       }
     } else {
-      ps.setVoltage(pandaState.voltage);
-      ps.setCurrent(pandaState.current);
+      ps.setVoltage(pandaState.voltage_pkt);
+      ps.setCurrent(pandaState.current_pkt);
     }
 
     ps.setIgnitionLine(pandaState.ignition_line);
