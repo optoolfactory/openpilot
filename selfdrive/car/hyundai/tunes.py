8b970831878fda2efc286ba1aaac493a52b6f667
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2022-04-22 02:51:37 +0900

    Update tunes.py

--- selfdrive/car/hyundai/tunes.py
+++ selfdrive/car/hyundai/tunes.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python3
 from enum import Enum
-
+from common.params import Params
+from decimal import Decimal
 
 class LongTunes(Enum):
   PEDAL = 0
-  TSS2 = 1
-  TSS = 2
 
 class LatTunes(Enum):
   INDI = 0
   LQR = 1
-  PID_A = 2
-  PID_B = 3
-  PID_C = 4
-  PID_D = 5
-  PID_E = 6
-  PID_F = 7
-  PID_G = 8
-  PID_I = 9
+  PID = 2
+  PID_A = 3
+  PID_B = 4
+  PID_C = 5
+  PID_D = 6
+  PID_E = 7
+  PID_F = 8
+  PID_G = 9
   PID_H = 10
-  PID_J = 11
-  PID_K = 12
-  PID_L = 13
-  PID_M = 14
-  PID_N = 15
+  PID_I = 11
+  PID_J = 12
+  PID_K = 13
+  PID_L = 14
+  PID_M = 15
   TORQUE = 16
 
 
 ###### LONG ######
 def set_long_tune(tune, name):
   # Improved longitudinal tune
-  if name == LongTunes.TSS2 or name == LongTunes.PEDAL:
+  if name == LongTunes.PEDAL:
     tune.deadzoneBP = [0., 8.05]
     tune.deadzoneV = [.0, .14]
     tune.kpBP = [0., 5., 20.]
@@ -38,81 +37,158 @@ def set_long_tune(tune, name):
     tune.kiBP = [0., 5., 12., 20., 27.]
     tune.kiV = [.35, .23, .20, .17, .1]
   # Default longitudinal tune
-  elif name == LongTunes.TSS:
-    tune.deadzoneBP = [0., 9.]
-    tune.deadzoneV = [0., .15]
-    tune.kpBP = [0., 5., 35.]
-    tune.kiBP = [0., 35.]
-    tune.kpV = [3.6, 2.4, 1.5]
-    tune.kiV = [0.54, 0.36]
   else:
     raise NotImplementedError('This longitudinal tune does not exist')
 
 
 ###### LAT ######
 def set_lat_tune(tune, name, MAX_TORQUE=2.5, FRICTION=.1):
+  params = Params()
   if name == LatTunes.TORQUE:
+    TorqueKp = float(Decimal(params.get("TorqueKp", encoding="utf8")) * Decimal('0.1'))
+    TorqueKf = float(Decimal(params.get("TorqueKf", encoding="utf8")) * Decimal('0.1'))
+    TorqueKi = float(Decimal(params.get("TorqueKi", encoding="utf8")) * Decimal('0.1'))
     tune.init('torque')
     tune.torque.useSteeringAngle = True
-    tune.torque.kp = 2.0 / MAX_TORQUE
-    tune.torque.kf = 1.0 / MAX_TORQUE
-    tune.torque.ki = 0.5 / MAX_TORQUE
+    tune.torque.kp = TorqueKp / MAX_TORQUE # 2.0
+    tune.torque.kf = TorqueKf / MAX_TORQUE # 1.0
+    tune.torque.ki = TorqueKi / MAX_TORQUE # 0.5
     tune.torque.friction = FRICTION
+  elif name == LatTunes.LQR:
+    Scale = float(Decimal(params.get("Scale", encoding="utf8")) * Decimal('1.0'))
+    LqrKi = float(Decimal(params.get("LqrKi", encoding="utf8")) * Decimal('0.001'))
+    DcGain = float(Decimal(params.get("DcGain", encoding="utf8")) * Decimal('0.00001'))
+    tune.init('lqr')
+    tune.lqr.scale = Scale
+    tune.lqr.ki = LqrKi
+    tune.lqr.a = [0., 1., -0.22619643, 1.21822268]
+    tune.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+    tune.lqr.c = [1., 0.]
+    tune.lqr.k = [-110., 451.]
+    tune.lqr.l = [0.33, 0.318]
+    tune.lqr.dcGain = DcGain
   elif name == LatTunes.INDI:
+    InnerLoopGain = float(Decimal(params.get("InnerLoopGain", encoding="utf8")) * Decimal('0.1'))
+    OuterLoopGain = float(Decimal(params.get("OuterLoopGain", encoding="utf8")) * Decimal('0.1'))
+    TimeConstant = float(Decimal(params.get("TimeConstant", encoding="utf8")) * Decimal('0.1'))
+    ActuatorEffectiveness = float(Decimal(params.get("ActuatorEffectiveness", encoding="utf8")) * Decimal('0.1'))
     tune.init('indi')
     tune.indi.innerLoopGainBP = [0.]
-    tune.indi.innerLoopGainV = [4.0]
+    tune.indi.innerLoopGainV = [InnerLoopGain] # 4.0, third tune. Highest value that still gives smooth control. Effects turning into curves.
     tune.indi.outerLoopGainBP = [0.]
-    tune.indi.outerLoopGainV = [3.0]
+    tune.indi.outerLoopGainV = [OuterLoopGain] # 3.0, forth tune. Highest value that still gives smooth control. Effects lane centering.
     tune.indi.timeConstantBP = [0.]
-    tune.indi.timeConstantV = [1.0]
+    tune.indi.timeConstantV = [TimeConstant] # 1.0, second tune. Lowest value with smooth actuation. Avoid the noise of actuator gears thrashing.
     tune.indi.actuatorEffectivenessBP = [0.]
-    tune.indi.actuatorEffectivenessV = [1.0]
+    tune.indi.actuatorEffectivenessV = [ActuatorEffectiveness] # 1.0, first tune. Lowest value without oversteering. May vary with speed.
+    # actuatorEffectiveness
+      # As effectiveness increases, actuation strength decreases
+      # Too high: weak, sloppy lane centering, slow oscillation, can't follow high curvature, high steering error causes snappy corrections
+      # Too low: overpower, saturation, jerky, fast oscillation
+      # Just right: Highest still able to maintain good lane centering.
+    # timeConstant
+      # Extend exponential decay of prior output steer
+      # Too high: sloppy lane centering
+      # Too low: noisy actuation, responds to every bump, maybe unable to maintain lane center due to rapid actuation
+      # Just right: above noisy actuation and lane centering instability
+    # innerLoopGain
+      # Steer rate error gain
+      # Too high: jerky oscillation in high curvature
+      # Too low: sloppy, cannot accomplish desired steer angle
+      # Just right: brief snap on entering high curvature
+    # outerLoopGain
+      # Steer error gain
+      # Too high: twitchy hyper lane centering, oversteering
+      # Too low: sloppy, all over lane
+      # Just right: crisp lane centering
   elif 'PID' in str(name):
-    tune.init('pid')
-    tune.pid.kiBP = [0.0]
-    tune.pid.kpBP = [0.0]
-    if name == LatTunes.PID_A:
+    if name == LatTunes.PID:
+      PidKp = float(Decimal(params.get("PidKp", encoding="utf8")) * Decimal('0.01'))
+      PidKi = float(Decimal(params.get("PidKi", encoding="utf8")) * Decimal('0.001'))
+      PidKd = float(Decimal(params.get("PidKd", encoding="utf8")) * Decimal('0.01'))
+      PidKf = float(Decimal(params.get("PidKf", encoding="utf8")) * Decimal('0.00001'))
+      tune.init('pid')
+      tune.pid.kpBP = [0., 9.]
+      tune.pid.kpV = [0.1, PidKp]
+      tune.pid.kiBP = [0., 9.]
+      tune.pid.kiV = [0.01, PidKi]
+      tune.pid.kdBP = [0.]
+      tune.pid.kdV = [PidKd]
+      tune.pid.kf = PidKf
+    elif name == LatTunes.PID_A:
+      tune.init('pid')
+      tune.pid.kiBP = [0.0]
+      tune.pid.kpBP = [0.0]
       tune.pid.kpV = [0.2]
       tune.pid.kiV = [0.05]
       tune.pid.kf = 0.00003
     elif name == LatTunes.PID_C:
+      tune.init('pid')
+      tune.pid.kiBP = [0.0]
+      tune.pid.kpBP = [0.0]
       tune.pid.kpV = [0.6]
       tune.pid.kiV = [0.1]
       tune.pid.kf = 0.00006
     elif name == LatTunes.PID_D:
+      tune.init('pid')
+      tune.pid.kiBP = [0.0]
+      tune.pid.kpBP = [0.0]
       tune.pid.kpV = [0.6]
       tune.pid.kiV = [0.1]
       tune.pid.kf = 0.00007818594
     elif name == LatTunes.PID_F:
+      tune.init('pid')
+      tune.pid.kiBP = [0.0]
+      tune.pid.kpBP = [0.0]
       tune.pid.kpV = [0.723]
       tune.pid.kiV = [0.0428]
       tune.pid.kf = 0.00006
     elif name == LatTunes.PID_G:
+      tune.init('pid')
+      tune.pid.kiBP = [0.0]
+      tune.pid.kpBP = [0.0]
       tune.pid.kpV = [0.18]
       tune.pid.kiV = [0.015]
       tune.pid.kf = 0.00012
     elif name == LatTunes.PID_H:
+      tune.init('pid')
+      tune.pid.kiBP = [0.0]
+      tune.pid.kpBP = [0.0]
       tune.pid.kpV = [0.17]
       tune.pid.kiV = [0.03]
       tune.pid.kf = 0.00006
     elif name == LatTunes.PID_I:
+      tune.init('pid')
+      tune.pid.kiBP = [0.0]
+      tune.pid.kpBP = [0.0]
       tune.pid.kpV = [0.15]
       tune.pid.kiV = [0.05]
       tune.pid.kf = 0.00004
     elif name == LatTunes.PID_J:
+      tune.init('pid')
+      tune.pid.kiBP = [0.0]
+      tune.pid.kpBP = [0.0]
       tune.pid.kpV = [0.19]
       tune.pid.kiV = [0.02]
       tune.pid.kf = 0.00007818594
     elif name == LatTunes.PID_L:
+      tune.init('pid')
+      tune.pid.kiBP = [0.0]
+      tune.pid.kpBP = [0.0]
       tune.pid.kpV = [0.3]
       tune.pid.kiV = [0.05]
       tune.pid.kf = 0.00006
     elif name == LatTunes.PID_M:
+      tune.init('pid')
+      tune.pid.kiBP = [0.0]
+      tune.pid.kpBP = [0.0]
       tune.pid.kpV = [0.3]
       tune.pid.kiV = [0.05]
       tune.pid.kf = 0.00007
     elif name == LatTunes.PID_N:
+      tune.init('pid')
+      tune.pid.kiBP = [0.0]
+      tune.pid.kpBP = [0.0]
       tune.pid.kpV = [0.35]
       tune.pid.kiV = [0.15]
       tune.pid.kf = 0.00007818594
