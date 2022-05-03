9bf0fce65377795ffbd729360a77c0dbd8d62d93
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2022-05-03 21:42:24 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1015,7 +1015,7 @@ static void bb_ui_draw_measures_left(UIState *s, int bb_x, int bb_y, int bb_w ) 
     } else {
       snprintf(uom_str, sizeof(uom_str), "mi/h");
     }
-    bb_ry +=bb_ui_draw_measure(s, val_str, uom_str, "REL SPD",
+    bb_ry +=bb_ui_draw_measure(s, val_str, uom_str, "상대REL SPD",
         bb_rx, bb_ry, bb_uom_dx,
         val_color, lab_color, uom_color,
         value_fontSize, label_fontSize, uom_fontSize, 0);
