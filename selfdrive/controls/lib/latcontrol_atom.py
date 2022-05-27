d2238964b8d0394f033d069f1a81e5ec30089b64
multikyd <72329880+multikyd@users.noreply.github.com>
2022-05-27 16:33:41 +0900

    kyd

--- selfdrive/controls/lib/latcontrol_atom.py
+++ selfdrive/controls/lib/latcontrol_atom.py
@@ -219,6 +219,12 @@ class LatControlATOM(LatControl):
           delta1 = abs(lat1 - self.output_torque)
           delta2 = abs(lat2 - self.output_torque)
           selected = self.multi_lat_ang_opt[2] if delta1 > delta2 else self.multi_lat_ang_opt[1]
+      
+      else:
+        lqr_output_torque, lqr_desired_angle, lqr_log  = self.LaLqr.update( active, CS, CP, VM, params, last_actuators, desired_curvature, desired_curvature_rate, llk )
+        toq_output_torque, toq_desired_angle, toq_log  = self.LaToq.update( active, CS, CP, VM, params, last_actuators, desired_curvature, desired_curvature_rate, llk )
+        ind_output_torque, ind_desired_angle, ind_log  = self.LaInd.update( active, CS, CP, VM, params, last_actuators, desired_curvature, desired_curvature_rate, llk )
+        pid_output_torque, pid_desired_angle, pid_log  = self.LaPid.update( active, CS, CP, VM, params, last_actuators, desired_curvature, desired_curvature_rate, llk )
 
 
       output_torque = interp( selected, [0, 1, 2, 3], [pid_output_torque, ind_output_torque, lqr_output_torque, toq_output_torque] )
