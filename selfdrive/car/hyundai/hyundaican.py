0e86de16abdb022cee28a819d4ea44941bd27980
multikyd <72329880+multikyd@users.noreply.github.com>
2022-05-29 15:31:45 +0900

    kyd

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -19,6 +19,7 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
   values["CF_Lkas_ActToi"] = steer_req and not cut_steer_temp
   values["CF_Lkas_ToiFlt"] = cut_steer_temp  # seems to allow actuation on CR_Lkas_StrToqReq
   values["CF_Lkas_MsgCount"] = frame % 0x10
+  values["CF_Lkas_Chksum"] = 0
 
   if car_fingerprint == CAR.GRANDEUR_HEV_IG:
     nSysWarnVal = 9
@@ -50,6 +51,8 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
 
   elif car_fingerprint == CAR.GENESIS_DH:
     values["CF_Lkas_LdwsActivemode"] = 2
+  elif car_fingerprint in (CAR.K5_HEV_JF, CAR.K5_JF, CAR.K7_HEV_YG, CAR.K7_YG):
+    values["CF_Lkas_LdwsActivemode"] = 0
 
   if ldws:
   	values["CF_Lkas_LdwsOpt_USM"] = 3
