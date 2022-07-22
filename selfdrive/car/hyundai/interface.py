42c41af8cd8eb50965aac4c69c022632088b5210
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2022-07-22 12:40:12 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -203,6 +203,9 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.NEXO_FE:
       ret.mass = 1885. + STD_CARGO_KG
       ret.wheelbase = 2.79
+    elif candidate == CAR.MAXCRUZE:
+      ret.mass = 1885. + STD_CARGO_KG
+      ret.wheelbase = 2.90      
     # kia
     elif candidate == CAR.KIA_FORTE:
       ret.mass = 3558. * CV.LB_TO_KG
